bc935d1bf97143fd2e558360eff9fd1096b5883d
Joice V Joseph <joice.joseph3198@gmail.com>
2021-11-28 17:23:10 +0530

    to produce filtered spectogram for audio sample

--- audio_fingerprinting.py
+++ audio_fingerprinting.py
@@ -15,25 +15,66 @@ import librosa.display
 # hop_length: number of samples between successive frames. See librosa.core.stft
 
 # win_length: Each frame of audio is windowed by window().
-filename = "sample_music1.mp3"
-y,sr = lr.load(filename,sr=11025) # Returns: y:np.ndarray [shape=(n_mels, t)]
+filename = "sample_music.mp3"
+y,sr = lr.load(filename) 
+time_res = 1024*0.5/sr
+slices = np.abs(librosa.stft(y, n_fft=1024, hop_length=512, win_length=1024, window='hann'))
+freq_bins, time_slices = slices.shape
 
-window_size = 1024
-window = np.hanning(window_size)
-stft  = librosa.core.spectrum.stft(y, n_fft=window_size, hop_length=512, window=window)
+logarithmic_bands = [(0,20), (20,60), (60,120), (120,240), (240,512)]
+nbands = len(logarithmic_bands)
+
+#stores the final frequency points for each time slice in a dictionary with key=time_slice and value=list of strong frequencies
+filtered_spectogram = {}
+band_across_time_mean = np.zeros((time_slices, nbands))
 
 
+#iterate over each time slice
+for time_slice in range(time_slices):
+    temp_spectrogram = []
+    #store temporary spectograms which need to be filtered further
+	#contains list of (time_of_occurence, frequency_bin, logarithmic_band_index)
+    slice_mean = 0
 
-for each in stft:
-    out = 2 * np.abs(each) / np.sum(window)
-    fig = plt.figure(figsize=(10, 4))
-    ax = fig.add_subplot(111)
-    librosa.display.specshow(librosa.amplitude_to_db(out, ref=np.max), ax=ax, y_axis='log', x_axis='time')
-    plt.title('Mel spectrogram')
-    plt.tight_layout()
-    plt.show()
-# Compute a mel-scaled spectrogram.
-# If a spectrogram input S is provided, then it is mapped directly onto the mel basis by mel_f.dot(S).
-# If a time-series input y, sr is provided, then its magnitude spectrogram S is first computed, and then mapped onto the mel scale by mel_f.dot(S**power).
-# By default, power=2 operates on a power spectrum.
+    for i in range(nbands):
+        logband = logarithmic_bands[i]
+        fft_slice = slices[logband[0]:logband[1],time_slice]
+        idx = np.argmax(fft_slice) # max freq in the band
+        band_across_time_mean[time_slice][i] = fft_slice[idx] # mean for the band
+        slice_mean += fft_slice[idx]# mean of the slice
+        temp_spectrogram.append((time_slice,logband[0]+idx,fft_slice[idx],i))
+        
 
+    slice_mean /= nbands
+
+    for poss in temp_spectrogram:
+        if not poss[0] in filtered_spectogram.keys():
+            filtered_spectogram[poss[0]] = []
+
+        if poss[2] >= slice_mean:
+            filtered_spectogram[poss[0]].append((poss[1],poss[2],poss[3]))
+
+
+# Drop points which are weaker than the mean in it's band
+band_across_time_mean = np.mean(band_across_time_mean, axis=0)
+
+for time_slice, data in filtered_spectogram.items(): # time_slice,band[0]+idx,fft_slice[idx],i
+    final_f=[]   
+    for band in data:
+        f_bin, mag, band_no = band[0], band[1], band[2]
+        if mag >= band_across_time_mean[band_no]: # if freq >= mean of its band
+            final_f.append(f_bin)
+    
+    filtered_spectogram[time_slice] = final_f
+
+
+to_plot_x, to_plot_y = [], []
+for key, val in filtered_spectogram.items():
+    plt.scatter(np.array([key]*len(val))*time_res, val, marker='x', c='r')
+
+for log_band in logarithmic_bands:
+    plt.axhline(y=log_band[0], color='k', linestyle='-')
+    
+plt.ylim(0, freq_bins)
+plt.show()
+    
